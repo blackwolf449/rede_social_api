9c7850f24898260582a85a6df0ccb6b714fb41ee
blackwolf449 <tacvini@gmail.com>
2022-09-10 21:08:05 -0300

    update comments route

--- core/routes/comments.js
+++ core/routes/comments.js
@@ -4,7 +4,6 @@ import { validateBody } from '../methods/validate.js'
 import { create, get } from '../database/comments.js'
 import { get as getToken } from '../database/tokens.js'
 import { get as getUser } from '../database/user.js'
-import { badRequest } from '../methods/responses.js'
 
 const router = express.Router()
 export { router as CommentsRouter }
@@ -14,8 +13,6 @@ router.post(
     validateBody({ text: true, title: true }),
     authenticate(),
     async (req, res) => {
-        const exist = await get('title', req.body.title)
-        if (exist) return badRequest(res, 'This title already exist')
         const token = await getToken(
             'accessToken',
             req.headers['authorization'].split(' ')[1]
